d9ae9dbb9c854e05351bd84da0467afee53fc589
imVrishabh <vrishabh0280@gmail.com>
2026-07-26 00:11:18 +0530

    intial commit

---
+++ problem1.py
@@ -0,0 +1,10 @@
+# print a poem
+
+print('''Twinkle, twinkle, dragon bright,
+Soaring softly through the night.
+Mermaids sing and fairies play,
+Guiding dreams till break of day.
+Phoenix rising, flames that glow,
+Kitsune’s lantern lights the snow.
+Twinkle, twinkle, magic near,
+Folklore whispers, dreams appear.''')
---
+++ problrm3.py
@@ -0,0 +1,4 @@
+import pyttsx3 
+engine = pyttsx3.init()
+engine.say(""" hi vrishabh sir i am your student from code with harry""")
+engine.runAndWait()
