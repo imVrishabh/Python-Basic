f73fefc30c64302fcd41d0a686fdf09a86303689
imVrishabh <vrishabh0280@gmail.com>
2026-07-27 17:54:06 +0530

    Added logic file

--- Chapter 5(DIctionary&sets)/02_divt_method.py
+++ Chapter 5(DIctionary&sets)/02_divt_method.py
@@ -3,10 +3,9 @@ marks={
     "yt":49,
     "md":54
 }
-# print(marks.items())
-# print(marks.keys())
-# print(marks.values())
-# marks.update ({"vd":72 , "v":100})
-# print(marks)
+print(marks.items())
+print(marks.keys())
+print(marks.values())
+marks.update ({"vd":72 , "v":100})
+print(marks)
 print(marks.get("vd"))
-print
