6a8d0a9641fc5f6a45b764c67112abcd65700459
imVrishabh <vrishabh0280@gmail.com>
2026-07-27 17:59:08 +0530

    resolve logic

--- Chapter 2 (Varibles & Data type)/01_varibles.py
+++ Chapter 2 (Varibles & Data type)/01_varibles.py
@@ -1,7 +1,8 @@
 a=1
-# integers me 1234.. aata hai
+# ''integers me 1234.. aata hai
 # float me 1.1, 2.2 aata hai
-# double couts".." ke andar jo bhi likhenge vo string me convert ho jayega
+# double couts".." ke andar jo bhi likhenge vo string me convert ho jayega  
+
 b=2
 
 c="vrishabh"
