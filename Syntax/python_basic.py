54117a3584dc42f19ceeb6e203d2741b30b4f44d
imVrishabh <vrishabh0280@gmail.com>
2026-08-15 18:35:24 +0530

    Added for loop synatx

--- Syntax/python_basic.py
+++ Syntax/python_basic.py
@@ -3,3 +3,12 @@ spam = 0
 while spam <5:
     print("Hello world")
     spam =spam +1
+
+
+'''///'''
+
+for i in range[1,2,3,4,5]:
+    if i ==3:
+        break
+else:
+    print("only Executed when no item of the list is equal to 3")
