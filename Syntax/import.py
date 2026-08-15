42f595cbdafc06a64e183e3db16821f060e9b20e
imVrishabh <vrishabh0280@gmail.com>
2026-08-15 18:43:30 +0530

    Added import method

---
+++ Syntax/import.py
@@ -0,0 +1,17 @@
+import random
+for i in range(5):
+    print(random.randint(1,10))
+
+# import anything using this [ form random import *]
+
+'''////'''
+''' If want to end a program early use this '''
+import sys
+
+while True:
+    print('True exit to exit.')
+    response = input()
+    if response == 'exit':
+        sys.exit()
+    print('You typed {}.'.format(response))
+    
